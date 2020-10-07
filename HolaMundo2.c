d86fb99d1ecc9bf8fdae88d7ca3c6d129a909abe
sofiaCorzas <72525164+sofiaCorzas@users.noreply.github.com>
2020-10-07 16:39:46 -0500

    Update HolaMundo2.c

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -3,5 +3,6 @@ int main(){
 printf("este es mi segundo codigo");
 printf("programar es facil");
 printf("voy a compartir con mi compañero el codigo");
+printf("\nCorzas Rodriguez Sofia\n");
 return 0;
 }
